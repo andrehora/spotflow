ec2a17a2a26a7788075a8c32d0361fcdbc5e467c
andrehora <andrehoraa@gmail.com>
2021-10-11 18:10:30 -0300

    pass

--- happyflow/html_report.py
+++ happyflow/html_report.py
@@ -17,26 +17,26 @@ class HTMLReport:
         for flow in self.analysis.most_common_flow(n):
             pass
 
-    def show_code(self, flow_number=0, filename='example.html'):
-
-        flow = self.flow_result.flows[flow_number]
-        # state_result = flow.state_result
-        flow_lines = flow.run_lines
-
-        content = read_file(self.target_entity.filename)
-        html_lines = html_lines_for_code(content)
-        current_line = 0
-        lines = []
-        for line in html_lines:
-            current_line += 1
-            if self.target_entity.has_line(current_line):
-
-                if current_line in flow_lines:
-                    line = f'<span class="happy">{line}</span>'
-                if current_line not in flow_lines:
-                    line = f'<span class="alter">{line}</span>'
-                line = f'<span>{str(current_line)}</span>{line}'
-                lines.append(line)
-
-        html = self.source_tmpl.render({'lines': lines})
-        write_html('./happyflow/html/example.html', html)
+    # def show_code(self, flow_number=0, filename='example.html'):
+    #
+    #     flow = self.flow_result.flows[flow_number]
+    #     # state_result = flow.state_result
+    #     flow_lines = flow.run_lines
+    #
+    #     content = read_file(self.target_entity.filename)
+    #     html_lines = html_lines_for_code(content)
+    #     current_line = 0
+    #     lines = []
+    #     for line in html_lines:
+    #         current_line += 1
+    #         if self.target_entity.has_line(current_line):
+    #
+    #             if current_line in flow_lines:
+    #                 line = f'<span class="happy">{line}</span>'
+    #             if current_line not in flow_lines:
+    #                 line = f'<span class="alter">{line}</span>'
+    #             line = f'<span>{str(current_line)}</span>{line}'
+    #             lines.append(line)
+    #
+    #     html = self.source_tmpl.render({'lines': lines})
+    #     write_html('./happyflow/html/example.html', html)
