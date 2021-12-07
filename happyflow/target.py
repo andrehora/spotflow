f44dce8049455966ec278ba0c6f231ad1e1272f6
andrehora <andrehoraa@gmail.com>
2021-12-07 16:02:47 -0300

    dealing with cache...

--- happyflow/target.py
+++ happyflow/target.py
@@ -16,7 +16,10 @@ class TargetMethod:
         self.start_line = None
         self.end_line = None
         self.info = None
+
         self.code_lines = None
+        self.html_lines = None
+        self.plain_code = None
 
     def executable_lines(self):
         exec_lines = self.ensure_executable_lines_for_file()
@@ -31,9 +34,6 @@ class TargetMethod:
     def executable_lines_count(self):
         return len(self.executable_lines())
 
-    def loc(self):
-        return self.end_line - self.start_line
-
     def line_is_executable(self, lineno):
         return lineno in self.executable_lines()
 
@@ -43,8 +43,19 @@ class TargetMethod:
     def has_lineno(self, lineno):
         return lineno in range(self.start_line, self.end_line + 1)
 
+    def is_method(self):
+        return self.class_name is not None
+
+    def is_func(self):
+        return not self.is_method()
+
+    def loc(self):
+        return self.end_line - self.start_line
+
     def get_code(self):
-        return ''.join(self.get_code_lines())
+        if not self.plain_code:
+            self.plain_code = ''.join(self.get_code_lines())
+        return self.plain_code
 
     def get_code_lines(self):
         if not self.code_lines:
@@ -52,13 +63,10 @@ class TargetMethod:
             self.code_lines = get_code_lines(self, file_lines)
         return self.code_lines
 
-    def ensure_file_lines(self):
-        if self.filename not in TargetMethod._code_lines:
-            TargetMethod._code_lines[self.filename] = read_file_lines(self.filename)
-        return TargetMethod._code_lines[self.filename]
-
     def get_html_lines(self):
-        return get_html_lines(self.get_code())
+        if not self.html_lines:
+            self.html_lines = get_html_lines(self.get_code())
+        return self.html_lines
 
     def get_code_line_at_lineno(self, n):
         return self.get_code_lines()[n-1]
@@ -66,17 +74,16 @@ class TargetMethod:
     def get_html_line_at_lineno(self, n):
         return self.get_html_lines()[n-1]
 
-    def summary(self):
-        return f'{self.full_name} (lines: {self.start_line}-{self.end_line})'
+    def ensure_file_lines(self):
+        if self.filename not in TargetMethod._code_lines:
+            TargetMethod._code_lines[self.filename] = read_file_lines(self.filename)
+        return TargetMethod._code_lines[self.filename]
 
     def full_name_escaped(self):
         return escape(self.full_name)
 
-    def is_method(self):
-        return self.class_name is not None
-
-    def is_func(self):
-        return not self.is_method()
+    def summary(self):
+        return f'{self.full_name} (lines: {self.start_line}-{self.end_line})'
 
     def __str__(self):
         return self.full_name
