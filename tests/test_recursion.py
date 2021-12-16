223878c0271b9e6560b064627332715b94142aec
andrehora <andrehoraa@gmail.com>
2021-12-16 16:44:20 -0300

    added recursion tests

---
+++ tests/test_recursion.py
@@ -0,0 +1,37 @@
+import unittest
+from tests.stub_test import TestRecursion
+from happyflow.api import run_and_flow_func
+
+
+class TestRecursiveCalls(unittest.TestCase):
+
+    def test_basic_recursion(self):
+        method_name = 'tests.stub_sut.Recursion.basic_recursion'
+        func = TestRecursion().test_basic_recursion
+
+        result = run_and_flow_func(func, [method_name])
+        self.assertEqual(len(result), 1)
+
+        calls = result[method_name].calls
+        self.assertEqual(len(calls), 3)
+
+        self.assertEqual(calls[0].run_lines, [582, 584])
+        self.assertEqual(calls[1].run_lines, [582, 584])
+        self.assertEqual(calls[2].run_lines, [582, 583])
+
+    def test_fib_recursive(self):
+        method_name = 'tests.stub_sut.Recursion.fib_recursive'
+        func = TestRecursion().test_fib_recursive_3
+
+        result = run_and_flow_func(func, [method_name])
+        self.assertEqual(len(result), 2)
+
+        calls = result['tests.stub_sut.Recursion.fib_recursive'].calls
+        self.assertEqual(len(calls), 1)
+
+        calls = result['tests.stub_sut.Recursion.fib_recursive.<locals>.fib_recursive_term'].calls
+        self.assertEqual(len(calls), 10)
+
+
+if __name__ == '__main__':
+    unittest.main()
