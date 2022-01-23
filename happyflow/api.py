58eee1ccd13b408f9c95a440e6d695f4dc042154
andrehora <andrehoraa@gmail.com>
2022-01-23 15:25:13 -0300

    target_files

--- happyflow/api.py
+++ happyflow/api.py
@@ -11,6 +11,9 @@ class HappyFlow:
     def target_methods(self, method_names):
         self.collector.method_names = method_names
 
+    def target_files(self, file_names):
+        self.collector.file_names = file_names
+
     def ignore_files(self, ignore_files):
         self.collector.ignore_files = ignore_files
 
@@ -44,31 +47,32 @@ class HappyFlow:
             return False
 
 
-def monitor(func, target_methods=None):
+def monitor(func, target_methods=None, target_files=None):
     hp = HappyFlow()
     hp.target_methods(target_methods)
+    hp.target_files(target_files)
+
     hp.start()
-
     func()
-
     hp.stop()
+
     return hp.result()
 
 
 def live(func, target_methods):
     hp = HappyFlow()
     hp.target_methods(target_methods)
+
     hp.start()
-
     func()
-
     hp.stop()
+
     hp.txt_report()
 
 
-def monitor_unittest_module(module, target_methods=None):
+def monitor_unittest_module(module, target_methods=None, target_files=None):
     suite = loadTestsFromModule(module)
     suite = suite_runner(suite)
-    return monitor(suite, target_methods)
+    return monitor(suite, target_methods, target_files)
 
 
