5c206d888b8b5c66ac017ad657778a46dd3b0072
andrehora <andrehoraa@gmail.com>
2022-12-18 10:18:50 -0300

    renamed monitor to monitor_func

--- tests/e2e/test_pprint.py
+++ tests/e2e/test_pprint.py
@@ -4,19 +4,19 @@ from spotflow.api import pprint
 
 class TestPrettyPrint(unittest.TestCase):
 
-    def test_pprint_count_uppercase_words_shortcut(self):
+    def test_pprint_count_uppercase_words(self):
 
         from tests.e2e.stub_funcs import inputs_count, count_uppercase_words
         ok = pprint(inputs_count, [count_uppercase_words])
         self.assertTrue(ok)
 
-    def test_pprint_parseparam_shortcut(self):
+    def test_pprint_parseparam(self):
 
         from tests.e2e.stub_funcs import inputs_parseparam, parseparam
         ok = pprint(inputs_parseparam, [parseparam])
         self.assertTrue(ok)
 
-    def test_pprint_splitparam_shortcut(self):
+    def test_pprint_splitparam(self):
 
         from tests.e2e.stub_funcs import inputs_splitparam, splitparam
         ok = pprint(inputs_splitparam, [splitparam])
