bfc91642fb1cc87d7864bc0dc6fd314a06e855ae
andrehora <andrehoraa@gmail.com>
2022-12-18 09:36:58 -0300

    working on command line interface

--- spotflow/cmdline.py
+++ spotflow/cmdline.py
@@ -7,18 +7,18 @@ from coverage.cmdline import PyRunner
 OK, ERR = 0, 1
 
 
-def parse_args():
+def parse_args(args=None):
 
     parser = argparse.ArgumentParser(description='Command line for SpotFlow')
 
     parser.add_argument('-o', '--objects', action='store_true',
-                        help='Objects.')
+                        help='Print summary of objects.')
 
     parser.add_argument('-c', '--calls', action='store_true',
-                        help='Calls.')
+                        help='Print summary of calls')
 
     parser.add_argument('-p', '--pprint', action='store_true',
-                        help='PPrint.')
+                        help='Pretty print code.')
 
     parser.add_argument('-s', '--script', type=str, nargs='?', const='spotflow_script.py',
                         help='Python script to be run after monitoring the program.'
@@ -46,42 +46,43 @@ def parse_args():
                              'To ignore multiple files, use multiple arguments, like -i file1 -i file2 -i ...')
 
     parser.add_argument('run',  type=str, nargs=argparse.REMAINDER,
-                        help='Command line to run, for example: "my_program.py", "pytest tests", "unittest discover", etc.')
+                        help='Command line to run, for example: '
+                             '"my_program.py", "pytest tests", "unittest discover", etc.'
+                             'This should be the last argument of command line.')
 
-    return parser.parse_args()
+    return parser.parse_args(args)
 
 
-class SpotFlowScript:
+class SpotFlowCommandLine:
 
-    def __init__(self):
-        args = parse_args()
+    def __init__(self, args=None):
 
-        self.objects = args.objects
-        self.calls = args.calls
-        self.pprint = args.pprint
+        parsed_args = parse_args(args)
 
-        # self.action = args.action
+        self.objects = parsed_args.objects
+        self.calls = parsed_args.calls
+        self.pprint = parsed_args.pprint
 
-        self.script = args.script
-        self.script_args = args.script_argument
+        self.script = parsed_args.script
+        self.script_args = parsed_args.script_argument
 
-        self.target_methods = args.target_method
-        self.target_method_names = args.target_method_name
+        self.target_methods = parsed_args.target_method
+        self.target_method_names = parsed_args.target_method_name
 
-        self.target_files = args.target_file
-        self.ignore_files = args.ignore_file
+        self.target_files = parsed_args.target_file
+        self.ignore_files = parsed_args.ignore_file
 
-        self.run_args = args.run
+        self.run_args = parsed_args.run
 
-    def command_line(self):
+    def run(self):
         if not self.run_args:
             print('Nothing to run...')
             return OK
 
         print(f"Running and monitoring: {' '.join(self.run_args)}")
-        return self.run()
+        return self.py_run()
 
-    def run(self):
+    def py_run(self):
 
         flow = SpotFlow()
         flow.target_methods(self.target_methods)
@@ -163,7 +164,7 @@ class SpotFlowScript:
 
 def main():
     try:
-        status = SpotFlowScript().command_line()
+        status = SpotFlowCommandLine().run()
     except Exception as e:
         print(e)
         status = ERR
