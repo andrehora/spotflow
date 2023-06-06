b6d59d3790768f8bdd11dab354c15e0f7bf0acd3
Andre Hora <andrehoraa@gmail.com>
2023-06-06 14:30:39 -0300

    removing lab

--- lab/polarity/polarity_run.py
+++
@@ -1,5 +0,0 @@
-from lab.polarity.polarity import compute_polarity
-
-
-from test import test_gzip as test
-compute_polarity('gzip', test, ['gzip'])
--- lab/rtmetrics/python_metrics.py
+++
@@ -1,5 +0,0 @@
-def metrics(method):
-    return \
-        ('method_name', method.full_name), \
-        ('calls', len(method.calls)), \
-        ('exceptions', len(method.exception_states()))
